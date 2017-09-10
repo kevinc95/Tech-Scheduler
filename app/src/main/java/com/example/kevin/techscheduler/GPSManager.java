ab3f67c78b92aa1eac4befe49ee680a69fc6dc9d
kevinc95 <kevinc95@vt.edu>
2017-09-10 02:11:36 -0400

    Update GPSManager.java

--- app/src/main/java/com/example/kevin/techscheduler/GPSManager.java
+++ app/src/main/java/com/example/kevin/techscheduler/GPSManager.java
@@ -10,28 +10,38 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 
 /**
- * Created by Kevin on 6/28/2017.
+ * Class for managing the GPS.
  */
-
 public class GPSManager implements LocationListener {
 
     MapsActivity mapsActivity;
     LocationManager locationManager;
     String LOCATIONPROVIDER=LocationManager.GPS_PROVIDER;
+    
+    /**
+     * Create an instance of GPSManager.
+     */
     public GPSManager(MapsActivity mapsActivity) {
         this.mapsActivity = mapsActivity;
         locationManager=(LocationManager)mapsActivity.getSystemService(Context.LOCATION_SERVICE);
     }
+    
+    /**
+     * Register the GPS and update the location every 5000 milliseconds
+     */
     public void register(){
         if ((ActivityCompat.checkSelfPermission(mapsActivity, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) &&
                 (ActivityCompat.checkSelfPermission(mapsActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
                         == PackageManager.PERMISSION_GRANTED)){
             locationManager.requestLocationUpdates(LOCATIONPROVIDER,5000,0,this);
-            //System.out.println("register updategps");
             mapsActivity.updateGPSLocation(locationManager.getLastKnownLocation(LOCATIONPROVIDER));
         }
     }
+    
+    /**
+     * Unregister the GPS
+     */
     public void unregister(){
         if ((ActivityCompat.checkSelfPermission(mapsActivity, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) &&
@@ -41,10 +51,12 @@ public class GPSManager implements LocationListener {
         }
     }
 
+    /**
+     * When location is changed, the mapsActivity will update location.
+     */
     @Override
     public void onLocationChanged(Location location) {
         mapsActivity.updateGPSLocation(location);
-        //System.out.println("change updategps");
     }
 
     @Override
